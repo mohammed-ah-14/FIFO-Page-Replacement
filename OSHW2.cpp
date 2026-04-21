d5238d5c5e6b2bab918f5f451096755e30396e6a
Mohammed Haris <mohammed.abdulharis@outlook.com>
2026-04-21 21:20:48 +0400

    skeleton code added

---
+++ OSHW2.cpp
@@ -0,0 +1,22 @@
+// OS Homework 2 - FIFO Page Replacement
+// Names:
+// IDs:
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // ----- Member 1 : Input & Setup -----
+
+    // ----- Member 2 : FIFO Logic (Pointer) -----
+
+    // ----- Member 3 : Page Fault Detection -----
+
+    // ----- Member 4 : Simulation Loop -----
+
+    // ----- Member 5 : Output Formatting -----
+
+    return 0;
+}
