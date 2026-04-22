e8d9147f6026723cfb45bd5339e33a4acac0f963
faaxr <farisalaga12@gmail.com>
2026-04-22 22:39:52 +0400

    Initialize frames and add member information
    Added member names and IDs to the header. Initialized frames array and added user input for number of frames.

--- OSHW2.cpp
+++ OSHW2.cpp
@@ -1,14 +1,26 @@
 // OS Homework 2 - FIFO Page Replacement
-// Names:
-// IDs:
+// Names:Faris munadhil
+// IDs:u23104246
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    int framesCount;
+    int referenceString[12] = {1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5};
+    int frames[100];
+    int pageFaults = 0;
 
-    // ----- Member 1 : Input & Setup -----
+    cout << "Enter number of frames: ";
+    cin >> framesCount;
+
+    for (int i = 0; i < framesCount; i++) {
+        frames[i] = -1;
+    }
+
+    
+  
 
     // ----- Member 2 : FIFO Logic (Pointer) -----
 
